6e5c9680e2998859ca52f8d91f99ef5e9d866bfe
Chris Mills <war.ruins+github@gmail.com>
2021-03-26 20:41:42 -0700

    Updates for the day.

--- Assets/Scripts/Settings/CustomGameSettings.cs
+++ Assets/Scripts/Settings/CustomGameSettings.cs
@@ -2,14 +2,11 @@
 {
     public static class CustomGameSettings
     {
-        // public enum ContentType { Loot, Quest }
-        
-        public const string QUEST_ASSETS_PATH = "Assets/Resources";
-        public const string ITEM_ASSETS_PATH = "Assets/Resources";
+        public const string QUEST_ASSETS_PATH = "Assets/Resources/Quests";
+        public const string ITEM_ASSETS_PATH = "Assets/Resources/Items";
         public const string STATIC_ASSETS_PATH = "Assets/StaticAssets";
         public const string TOOL_ICONS_PATH = STATIC_ASSETS_PATH + "/ToolIcons";
         public const string ITEM_CREATOR_ICON_PATH = TOOL_ICONS_PATH + "/loot.png";
         public const string QUEST_CREATOR_ICON_PATH = TOOL_ICONS_PATH + "/quest.png";
-    
     }
 }
